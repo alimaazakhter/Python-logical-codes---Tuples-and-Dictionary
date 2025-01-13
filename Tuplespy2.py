8634790a2115686382286e222806ebbbcf69140e
Ali Maaz Akhter <150456791+alimaazakhter@users.noreply.github.com>
2025-01-13 11:24:48 +0530

    Add files via upload

---
+++ Tuplespy2.py
@@ -0,0 +1,11 @@
+#using for and while loop to find range and length in tuples
+a = ("nokia", "apple", "oppo", "vivo")
+
+for i in range(len(a)):
+    print (i ,a[i])
+
+# i = 0
+# while i < len(a):
+#     print(a[i])
+#     i += 1
+
