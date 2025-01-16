e98cf53cfc8b38c925b01207bfd1d96ba1827efc
Ali Maaz Akhter <150456791+alimaazakhter@users.noreply.github.com>
2025-01-16 09:48:34 +0530

    Add files via upload

---
+++ DictSets1.py
@@ -0,0 +1,4 @@
+#Sets Example
+a = {"ironman", "hulk", "Caption America", "Venom", "Thor"}
+print(a)  # unordered collection of unique elements
+print(type(a))
---
+++ DictSets2.py
@@ -0,0 +1,15 @@
+#Functions of sets
+#add
+a = {"ironman", "hulk", "Caption America", "Venom", "Thor"}
+a.add("Black panther")
+print(a)
+# #pop
+a.pop()
+print(a)
+#remove
+a.remove("Venom")
+print(a)
+#copy
+a = {"ironman", "hulk", "Captain America", "Venom", "Thor"}
+b = a.copy()
+print(b)
